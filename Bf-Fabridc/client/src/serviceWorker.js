8b54791bc893f778c8b9aba0f6ba11425c52733b
Krutik Tejani <143246019+krutiktejani@users.noreply.github.com>
2026-02-11 20:07:04 +0530

    Update serviceWorker.js

--- Bf-Fabridc/client/src/serviceWorker.js
+++ Bf-Fabridc/client/src/serviceWorker.js
@@ -32,6 +32,13 @@ window.addEventListener('load', () => {
   }
 }
 
+export function register(config) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      return;
+    }
+
 window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
