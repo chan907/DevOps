c1953089c9dabfd4c7d28b2497e320d460365494
Krutik Tejani <143246019+krutiktejani@users.noreply.github.com>
2026-02-12 22:51:16 +0530

    Update serviceWorker.js

--- Bf-Fabridc/client/src/serviceWorker.js
+++ Bf-Fabridc/client/src/serviceWorker.js
@@ -55,3 +55,16 @@ window.addEventListener('load', () => {
           );
         });
       }
+
+  
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then(registration => {
+        registration.unregister();
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  }
+}
