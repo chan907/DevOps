5f67b084e67f22e24d02fcf452f17e64afb575cf
Krutik Tejani <143246019+krutiktejani@users.noreply.github.com>
2026-02-13 22:55:26 +0530

    Update serviceWorker.js

--- Bf-Fabridc/client/src/serviceWorker.js
+++ Bf-Fabridc/client/src/serviceWorker.js
@@ -1,8 +1,6 @@
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
@@ -43,11 +41,7 @@ window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
-
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
