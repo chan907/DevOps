47412e84cbac53ed2409f63941699d4fe290486f
Varad Mandhare <varadmandhare924@gmail.com>
2026-02-24 10:05:38 +0530

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,5 +1,4 @@
 
-
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
