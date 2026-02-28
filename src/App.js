9be90726f16e15d4c47a0f2edc9e9bb21b2a5660
chan907 <chanakshibedse13@gmail.com>
2026-02-28 16:05:51 +0530

    Added explanatory comment for useReducer state management

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Routes from "./components";
 import { LayoutContext, layoutState, layoutReducer } from "./components/shop";
 
 function App() {
-  const [data, dispatch] = useReducer(layoutReducer, layoutState);
-  return (
-    <Fragment>
-      <LayoutContext.Provider value={{ data, dispatch }}>
-        <Routes />
-      </LayoutContext.Provider>
-    </Fragment>
-  );
+    // Using useReducer for global state management across the application
+    const [data, dispatch] = useReducer(layoutReducer, layoutState);
+    return ( <
+        Fragment >
+        <
+        LayoutContext.Provider value = {
+            { data, dispatch } } >
+        <
+        Routes / >
+        <
+        /LayoutContext.Provider> <
+        /Fragment>
+    );
 }
 
 
