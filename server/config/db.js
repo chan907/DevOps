10ebc69d14abecf13830b1caaf6981772f36d771
“[varad <varadmandhare924@gmail.com>
2026-04-06 00:30:48 +0530

    fix: remove useCreateIndex from db.js and fix docker compose command in Jenkinsfile

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,6 @@ try {
   mongoose.connect(process.env.DATABASE || "mongodb://mongo:27017/ecommerce", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   });
   console.log("Database Connected Successfully");
 } catch (err) {
